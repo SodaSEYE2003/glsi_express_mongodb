ea50c6e27dfd961a2d05c904fbce823326f0cc45
SodaSEYE2003 <maremesodaseye37@gmail.com>
2024-04-28 00:40:12 +0200

    post and get

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -14,6 +14,17 @@ module.exports = app => {
   //Delete all Student 
   router.delete("/", students.deleteAll);
 
+  // Retrieve all registered Students
+  router.get("/registered", students.findAllRegistered);
+
+
+  // Retrieve a single Student with id
+  router.get("/:id", students.findOne);
+
+
+  // Update a Student with id
+ // router.put("/:id", students.update);
+
   app.use("/api/students", router);
 
 
